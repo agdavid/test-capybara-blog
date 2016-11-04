9cc964efd9b50e40846e8b78aac6995487af3908
agdavid <antonio.g.david@gmail.com>
2016-11-04 11:41:40 -0400

    add feature spec for blogs index

---
+++ spec/features/index_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "Index" do 
+    before do 
+        Blog.destroy_all
+    end
+
+    describe "visiting Blogs index" do 
+        it "shows the index of Blogs" do 
+            visit blogs_path
+            within 'h1' do 
+                expect(page).to have_content('Blogs')
+            end
+            within 'a' do 
+                expect(page).to have_content('New Blog')
+            end
+        end
+    end
+end
+
